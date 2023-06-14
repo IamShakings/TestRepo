10dee93154f03e293f922a0b64fb37c5a8d1496e
David Augusto <davidaugusto@hellotherma.com>
2023-06-14 13:50:37 +0800

    test

--- test_repo_action/HPA.py
+++ test_repo_action/HPA.py
@@ -105,15 +105,16 @@ def alert_on_hpa_reached_limit_2(event: HorizontalPodAutoscalerChangeEvent, acti
 
     avg_cpu = int(hpa.status.currentCPUUtilizationPercentage)
     new_max_replicas_suggestion = ceil((action_params.increase_pct + 100) * hpa.spec.maxReplicas / 100)
-    
-    label=f"Update HPA max replicas to: {new_max_replicas_suggestion}"(
-        action=scale_hpa_callback_2,
+
+
+    CallbackChoice(
+        action=scale_hpa_callback,
         action_params=ScaleHPAParams(
             max_replicas=new_max_replicas_suggestion,
         ),
-        kubernetes_object=hpa,
+        kubernetes_object=hpa
     )
-    
+
 
     finding = Finding(
         title=f"HPA-TEST *{event.obj.metadata.name}* in namespace *{event.obj.metadata.namespace}* reached max replicas: *{hpa.spec.maxReplicas}*",
@@ -125,7 +126,7 @@ def alert_on_hpa_reached_limit_2(event: HorizontalPodAutoscalerChangeEvent, acti
     finding.add_enrichment(
         [
             MarkdownBlock(f"On average, pods scaled under this HPA are using *{avg_cpu} %* of the requested cpu."),
-            CallbackBlock(label),
+            CallbackBlock(CallbackChoice),
         ]
     )
     event.add_finding(finding)
