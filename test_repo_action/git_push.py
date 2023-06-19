e1839629823249e8a4d6c0fb8f09917fa77bea53
David Augusto <davidaugusto@hellotherma.com>
2023-06-20 06:11:18 +0800

    modified hpa lines

--- test_repo_action/git_push.py
+++ test_repo_action/git_push.py
@@ -126,7 +126,7 @@ def git_push_changes(event: KubernetesAnyChangeEvent, action_params: GitAuditPar
         if event.operation == K8sOperationType.DELETE:
             git_repo.delete_push(path, name, f"Delete {path}/{name}", action_params.cluster_name)
         elif event.operation == K8sOperationType.CREATE:
-            obj_yaml = yaml.dump(hikaru.get_yaml(event.obj.spec))
+            obj_yaml = hikaru.get_yaml(event.obj.spec)
             # result = textwrap.dedent(hpa_yaml(name,obj_yaml))
             git_repo.commit_push(
                 obj_yaml,
@@ -139,8 +139,17 @@ def git_push_changes(event: KubernetesAnyChangeEvent, action_params: GitAuditPar
             old_spec = event.old_obj.spec if event.old_obj else None
             if obj_diff(event.obj.spec, old_spec, action_params.ignored_changes):  # we have a change in the spec
                 # result = textwrap.dedent(hpa_yaml(name,obj_yaml))
+                # Convert the Kubernetes object to YAML
+                obj_yaml = HikaruBase.get_yaml(hpa)
+
+                # Extract the desired fields
+                api_version = obj_yaml.get("apiVersion")
+                metadata_name = obj_yaml.get("metadata", {}).get("name")
+                specs = obj_yaml.get("spec")
+                logging.info(f"{api_version},{metadata_name},{specs}")
+
                 git_repo.commit_push(
-                    yaml.dump(hikaru.get_yaml(event.obj.spec)),
+                    obj_yaml,
                     path,
                     name,
                     f"Update {event.obj.kind} named {event.obj.metadata.name} on namespace {namespace}",
